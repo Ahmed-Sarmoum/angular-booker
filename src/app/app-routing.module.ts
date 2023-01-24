f6c5a412dfc3fb37d8afe4e82f04447c52915aca
Ahmed-Sarmoum <hamadadevloper93@gmail.com>
2023-01-24 21:21:30 +0100

    publish

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
 
+
 const routes: Routes = [
   {
     path: 'login', component: LoginComponent,
